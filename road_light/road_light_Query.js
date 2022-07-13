5bb9c4760a93e80a62e93932e013f2c049fc0ae6
ByeongsikK <qudtlr060@naver.com>
2022-07-13 15:51:04 +0900

    220713

--- road_light/road_light_Query.js
+++ road_light/road_light_Query.js
@@ -1,5 +1,113 @@
 $(document).ready(function () {
 
+    $.kakaomap = function() {
+        var mapContainer = document.getElementById('map'),
+        // 지도를 표시할 div 
+
+        mapOption = {
+            center: new kakao.maps.LatLng(36.46994680, 127.2691494), // 지도의 중심좌표
+            level: 3 // 지도의 확대 레벨    
+        };
+
+    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+
+    var positions = [{
+            title: '가로등1',
+            latlng: new kakao.maps.LatLng(36.46994680, 127.2691494)
+        },
+        {
+            title: '가로등2',
+            latlng: new kakao.maps.LatLng(36.46948720, 127.2689554)
+        },
+        {
+            title: '가로등3',
+            latlng: new kakao.maps.LatLng(36.46773580, 127.2678234)
+        },
+        {
+            title: '가로등4',
+            latlng: new kakao.maps.LatLng(36.47088880, 127.2698948)
+        }
+    ];
+
+    // 마커 이미지의 이미지 주소입니다
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+    for (var i = 0; i < positions.length; i++) {
+
+        // 마커 이미지의 이미지 크기 입니다
+        var imageSize = new kakao.maps.Size(24, 35);
+
+        // 마커 이미지를 생성합니다    
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng, // 마커를 표시할 위치
+            title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image: markerImage // 마커 이미지 
+        });
+
+    };
+    // -----카카오 맵-----
+
+    // ----- 지도 마커 클릭 이벤트-----
+    $(`img[title=가로등1`).click(function () {
+        $(`.photo_box.n2`).hide();
+        $(`.photo_box.n3`).hide();
+        $(`.photo_box.n4`).hide();
+        $(`.photo_box.n1`).show();
+        $(`.photo_box.n1`).css('display', 'flex');
+        $(`.garo0`).hide();
+        $(`.garo2`).hide();
+        $(`.garo3`).hide();
+        $(`.garo4`).hide();
+        $(`.garo1`).show();
+    });
+
+    $(`img[title=가로등2`).click(function () {
+        $(`.photo_box.n1`).hide();
+        $(`.photo_box.n3`).hide();
+        $(`.photo_box.n4`).hide();
+        $(`.photo_box.n2`).show();
+        $(`.photo_box.n2`).css('display', 'flex');
+        $(`.garo0`).hide();
+        $(`.garo1`).hide();
+        $(`.garo3`).hide();
+        $(`.garo4`).hide();
+        $(`.garo2`).show();
+    });
+
+    $(`img[title=가로등3`).click(function () {
+        $(`.photo_box.n1`).hide();
+        $(`.photo_box.n2`).hide();
+        $(`.photo_box.n4`).hide();
+        $(`.photo_box.n3`).show();
+        $(`.photo_box.n3`).css('display', 'flex');
+        $(`.garo0`).hide();
+        $(`.garo1`).hide();
+        $(`.garo2`).hide();
+        $(`.garo4`).hide();
+        $(`.garo3`).show();
+    });
+
+    $(`img[title=가로등4`).click(function () {
+        $(`.photo_box.n1`).hide();
+        $(`.photo_box.n2`).hide();
+        $(`.photo_box.n3`).hide();
+        $(`.photo_box.n4`).show();
+        $(`.photo_box.n4`).css('display', 'flex');
+        $(`.garo0`).hide();
+        $(`.garo1`).hide();
+        $(`.garo2`).hide();
+        $(`.garo3`).hide();
+        $(`.garo4`).show();
+    });
+    // ----- 지도 마커 클릭이벤트----- 
+
+    }
+    
 
     let login = false;
 
@@ -9,236 +117,23 @@ $(document).ready(function () {
 
     $(`.menu a:first`).click(function () {
         $(`body section`).hide();
-        if(login===false){
+        if (login === false) {
             $(`.sec1`).show();
         } else {
             $('.sec2').show();
-            $(`#map`).show(function () {
-                var mapContainer = document.getElementById('map'),
-                    // 지도를 표시할 div 
-        
-                    mapOption = {
-                        center: new kakao.maps.LatLng(36.46850856550445, 127.26871227961624), // 지도의 중심좌표
-                        level: 3 // 지도의 확대 레벨
-                    };
-        
-                var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-        
-        
-                var positions = [{
-                        title: '가로등1',
-                        latlng: new kakao.maps.LatLng(36.46994680, 127.2691494)
-                    },
-                    {
-                        title: '가로등2',
-                        latlng: new kakao.maps.LatLng(36.46948720, 127.2689554)
-                    },
-                    {
-                        title: '가로등3',
-                        latlng: new kakao.maps.LatLng(36.46773580, 127.2678234)
-                    },
-                    {
-                        title: '가로등4',
-                        latlng: new kakao.maps.LatLng(36.47088880, 127.2698948)
-                    }
-                ];
-        
-                // 마커 이미지의 이미지 주소입니다
-                var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-        
-                for (var i = 0; i < positions.length; i++) {
-        
-                    // 마커 이미지의 이미지 크기 입니다
-                    var imageSize = new kakao.maps.Size(24, 35);
-        
-                    // 마커 이미지를 생성합니다    
-                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-        
-                    // 마커를 생성합니다
-                    var marker = new kakao.maps.Marker({
-                        map: map, // 마커를 표시할 지도
-                        position: positions[i].latlng, // 마커를 표시할 위치
-                        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                        image: markerImage // 마커 이미지 
-                    });
-                   
-                };
-                // -----카카오 맵-----
-        
-                // ----- 지도 마커 클릭 이벤트-----
-                $(`img[title=가로등1`).click(function () {
-                    $(`.photo_box.n2`).hide();
-                    $(`.photo_box.n3`).hide();
-                    $(`.photo_box.n4`).hide();
-                    $(`.photo_box.n1`).show();
-                    $(`.photo_box.n1`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo2`).hide();
-                    $(`.garo3`).hide();
-                    $(`.garo4`).hide();
-                    $(`.garo1`).show();
-                });
-        
-                $(`img[title=가로등2`).click(function () {
-                    $(`.photo_box.n1`).hide();
-                    $(`.photo_box.n3`).hide();
-                    $(`.photo_box.n4`).hide();
-                    $(`.photo_box.n2`).show();
-                    $(`.photo_box.n2`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo1`).hide();
-                    $(`.garo3`).hide();
-                    $(`.garo4`).hide();
-                    $(`.garo2`).show();
-                });
-        
-                $(`img[title=가로등3`).click(function () {
-                    $(`.photo_box.n1`).hide();
-                    $(`.photo_box.n2`).hide();
-                    $(`.photo_box.n4`).hide();
-                    $(`.photo_box.n3`).show();
-                    $(`.photo_box.n3`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo1`).hide();
-                    $(`.garo2`).hide();
-                    $(`.garo4`).hide();
-                    $(`.garo3`).show();
-                });
-        
-                $(`img[title=가로등4`).click(function () {
-                    $(`.photo_box.n1`).hide();
-                    $(`.photo_box.n2`).hide();
-                    $(`.photo_box.n3`).hide();
-                    $(`.photo_box.n4`).show();
-                    $(`.photo_box.n4`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo1`).hide();
-                    $(`.garo2`).hide();
-                    $(`.garo3`).hide();
-                    $(`.garo4`).show();
-                });
-                // ----- 지도 마커 클릭 이벤트-----
-            });
+            $.kakaomap();
         }
         pgnum = 1
     });
-    
-    
+
+
     $(`.left_bt`).click(function () {
         $(`body section`).hide();
-        if(login===false){
+        if (login === false) {
             $(`.sec1`).show();
         } else {
             $('.sec2').show();
-            $(`#map`).show(function () {
-
-                var mapContainer = document.getElementById('map'),
-                    // 지도를 표시할 div 
-        
-                    mapOption = {
-                        center: new kakao.maps.LatLng(36.46850856550445, 127.26871227961624), // 지도의 중심좌표
-                        level: 3 // 지도의 확대 레벨
-                    };
-
-
-        
-                var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-  
-        
-                var positions = [{
-                        title: '가로등1',
-                        latlng: new kakao.maps.LatLng(36.46994680, 127.2691494)
-                    },
-                    {
-                        title: '가로등2',
-                        latlng: new kakao.maps.LatLng(36.46948720, 127.2689554)
-                    },
-                    {
-                        title: '가로등3',
-                        latlng: new kakao.maps.LatLng(36.46773580, 127.2678234)
-                    },
-                    {
-                        title: '가로등4',
-                        latlng: new kakao.maps.LatLng(36.47088880, 127.2698948)
-                    }
-                ];
-        
-                // 마커 이미지의 이미지 주소입니다
-                var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-        
-                for (var i = 0; i < positions.length; i++) {
-        
-                    // 마커 이미지의 이미지 크기 입니다
-                    var imageSize = new kakao.maps.Size(24, 35);
-        
-                    // 마커 이미지를 생성합니다    
-                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-        
-                    // 마커를 생성합니다
-                    var marker = new kakao.maps.Marker({
-                        map: map, // 마커를 표시할 지도
-                        position: positions[i].latlng, // 마커를 표시할 위치
-                        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                        image: markerImage // 마커 이미지 
-                    });
-                   
-                };
-                // -----카카오 맵-----
-        
-                // ----- 지도 마커 클릭 이벤트-----
-                $(`img[title=가로등1`).click(function () {
-                    $(`.photo_box.n2`).hide();
-                    $(`.photo_box.n3`).hide();
-                    $(`.photo_box.n4`).hide();
-                    $(`.photo_box.n1`).show();
-                    $(`.photo_box.n1`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo2`).hide();
-                    $(`.garo3`).hide();
-                    $(`.garo4`).hide();
-                    $(`.garo1`).show();
-                });
-        
-                $(`img[title=가로등2`).click(function () {
-                    $(`.photo_box.n1`).hide();
-                    $(`.photo_box.n3`).hide();
-                    $(`.photo_box.n4`).hide();
-                    $(`.photo_box.n2`).show();
-                    $(`.photo_box.n2`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo1`).hide();
-                    $(`.garo3`).hide();
-                    $(`.garo4`).hide();
-                    $(`.garo2`).show();
-                });
-        
-                $(`img[title=가로등3`).click(function () {
-                    $(`.photo_box.n1`).hide();
-                    $(`.photo_box.n2`).hide();
-                    $(`.photo_box.n4`).hide();
-                    $(`.photo_box.n3`).show();
-                    $(`.photo_box.n3`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo1`).hide();
-                    $(`.garo2`).hide();
-                    $(`.garo4`).hide();
-                    $(`.garo3`).show();
-                });
-        
-                $(`img[title=가로등4`).click(function () {
-                    $(`.photo_box.n1`).hide();
-                    $(`.photo_box.n2`).hide();
-                    $(`.photo_box.n3`).hide();
-                    $(`.photo_box.n4`).show();
-                    $(`.photo_box.n4`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo1`).hide();
-                    $(`.garo2`).hide();
-                    $(`.garo3`).hide();
-                    $(`.garo4`).show();
-                });
-                // ----- 지도 마커 클릭 이벤트-----
-            });
+            $.kakaomap();
         }
         pgnum = 1
     });
@@ -255,10 +150,10 @@ $(document).ready(function () {
         pgnum = 2
     });
 
-    
+
     $(`.my_minwon`).click(function () {
         $('body > section').hide();
-        if(login===false){
+        if (login === false) {
             $(`.sec1`).show();
         }
     });
@@ -271,113 +166,8 @@ $(document).ready(function () {
         if (pgnum === 1) {
             $('body > section').hide();
             $(`.sec2`).show();
-            $(`#map`).show(function () {
-                var mapContainer = document.getElementById('map'),
-                    // 지도를 표시할 div 
-        
-                    mapOption = {
-                        center: new kakao.maps.LatLng(36.46850856550445, 127.26871227961624), // 지도의 중심좌표
-                        level: 3 // 지도의 확대 레벨
-                    };
-        
-                var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-        
-        
-                var positions = [{
-                        title: '가로등1',
-                        latlng: new kakao.maps.LatLng(36.46994680, 127.2691494)
-                    },
-                    {
-                        title: '가로등2',
-                        latlng: new kakao.maps.LatLng(36.46948720, 127.2689554)
-                    },
-                    {
-                        title: '가로등3',
-                        latlng: new kakao.maps.LatLng(36.46773580, 127.2678234)
-                    },
-                    {
-                        title: '가로등4',
-                        latlng: new kakao.maps.LatLng(36.47088880, 127.2698948)
-                    }
-                ];
-        
-                // 마커 이미지의 이미지 주소입니다
-                var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-        
-                for (var i = 0; i < positions.length; i++) {
-        
-                    // 마커 이미지의 이미지 크기 입니다
-                    var imageSize = new kakao.maps.Size(24, 35);
-        
-                    // 마커 이미지를 생성합니다    
-                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-        
-                    // 마커를 생성합니다
-                    var marker = new kakao.maps.Marker({
-                        map: map, // 마커를 표시할 지도
-                        position: positions[i].latlng, // 마커를 표시할 위치
-                        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                        image: markerImage // 마커 이미지 
-                    });
-                   
-                };
-                // -----카카오 맵-----
-        
-                // ----- 지도 마커 클릭 이벤트-----
-                $(`img[title=가로등1`).click(function () {
-                    $(`.photo_box.n2`).hide();
-                    $(`.photo_box.n3`).hide();
-                    $(`.photo_box.n4`).hide();
-                    $(`.photo_box.n1`).show();
-                    $(`.photo_box.n1`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo2`).hide();
-                    $(`.garo3`).hide();
-                    $(`.garo4`).hide();
-                    $(`.garo1`).show();
-                });
-        
-                $(`img[title=가로등2`).click(function () {
-                    $(`.photo_box.n1`).hide();
-                    $(`.photo_box.n3`).hide();
-                    $(`.photo_box.n4`).hide();
-                    $(`.photo_box.n2`).show();
-                    $(`.photo_box.n2`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo1`).hide();
-                    $(`.garo3`).hide();
-                    $(`.garo4`).hide();
-                    $(`.garo2`).show();
-                });
-        
-                $(`img[title=가로등3`).click(function () {
-                    $(`.photo_box.n1`).hide();
-                    $(`.photo_box.n2`).hide();
-                    $(`.photo_box.n4`).hide();
-                    $(`.photo_box.n3`).show();
-                    $(`.photo_box.n3`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo1`).hide();
-                    $(`.garo2`).hide();
-                    $(`.garo4`).hide();
-                    $(`.garo3`).show();
-                });
-        
-                $(`img[title=가로등4`).click(function () {
-                    $(`.photo_box.n1`).hide();
-                    $(`.photo_box.n2`).hide();
-                    $(`.photo_box.n3`).hide();
-                    $(`.photo_box.n4`).show();
-                    $(`.photo_box.n4`).css('display', 'flex');
-                    $(`.garo0`).hide();
-                    $(`.garo1`).hide();
-                    $(`.garo2`).hide();
-                    $(`.garo3`).hide();
-                    $(`.garo4`).show();
-                });
-                // ----- 지도 마커 클릭 이벤트-----
-            });
             console.log(pgnum)
+            $.kakaomap();
         } else if (pgnum === 2) {
             console.log(pgnum)
             $('body > section').hide();
@@ -399,19 +189,19 @@ $(document).ready(function () {
 
 
 
-     // -----처리현황 페이징 버튼-----
+    // -----처리현황 페이징 버튼-----
 
-     $('.pgnum:first-child').addClass('on');
+    $('.pgnum:first-child').addClass('on');
 
-     $('.pgnum').click(function () {
-         $('.pgnum').addClass('on')
-             .siblings().removeClass('on').end();
-     });
- 
-     $(`.pgnum`).click(function () {
-         $(this).addClass(`on`);
-     });
- 
-     // -----처리현황 페이징 버튼-----
+    $('.pgnum').click(function () {
+        $('.pgnum').addClass('on')
+            .siblings().removeClass('on').end();
+    });
+
+    $(`.pgnum`).click(function () {
+        $(this).addClass(`on`);
+    });
+
+    // -----처리현황 페이징 버튼-----
 
 });
